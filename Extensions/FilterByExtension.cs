3e79d173af7e8b4765bb5750b0c3eea89953ae3d
mumudevx <mumudevx@gmail.com>
2024-11-18 04:18:20 +0300

    FilterBy extension method was added with operator selection

---
+++ Extensions/FilterByExtension.cs
@@ -0,0 +1,98 @@
+﻿namespace GenericRepository.Extensions;
+
+public static class FilterByExtension
+{
+    public static IQueryable<T> FilterBy<T>(this IQueryable<T> collection, string filterBy) =>
+        ParseFilterBy(filterBy).Aggregate(collection, ApplyFilterBy);
+
+    private static IQueryable<T> ApplyFilterBy<T>(IQueryable<T> collection, FilterByInfo filterByInfo)
+    {
+        var type = typeof(T);
+        var arg = Expression.Parameter(type, "x");
+        var property = type.GetProperty(filterByInfo.PropertyName);
+        var propertyAccess = Expression.MakeMemberAccess(arg, property!);
+        var constant = Expression.Constant(Convert.ChangeType(filterByInfo.Value, property!.PropertyType));
+        var body = GetExpressionBody(filterByInfo.Operator, propertyAccess, constant);
+        var lambda = Expression.Lambda<Func<T, bool>>(body, arg);
+        return collection.Where(lambda);
+    }
+
+    private static Expression GetExpressionBody(FilterOperator filterOperator, Expression propertyAccess,
+        ConstantExpression constant)
+    {
+        return filterOperator switch
+        {
+            FilterOperator.Equal => Expression.Equal(propertyAccess, constant),
+            FilterOperator.NotEqual => Expression.NotEqual(propertyAccess, constant),
+            FilterOperator.GreaterThan => Expression.GreaterThan(propertyAccess, constant),
+            FilterOperator.GreaterThanOrEqual => Expression.GreaterThanOrEqual(propertyAccess, constant),
+            FilterOperator.LessThan => Expression.LessThan(propertyAccess, constant),
+            FilterOperator.LessThanOrEqual => Expression.LessThanOrEqual(propertyAccess, constant),
+            FilterOperator.Contains => Expression.Call(propertyAccess,
+                typeof(string).GetMethod("Contains", [typeof(string)])!, constant),
+            FilterOperator.StartsWith => Expression.Call(propertyAccess,
+                typeof(string).GetMethod("StartsWith", [typeof(string)])!, constant),
+            FilterOperator.EndsWith => Expression.Call(propertyAccess,
+                typeof(string).GetMethod("EndsWith", [typeof(string)])!, constant),
+            _ => throw new ArgumentOutOfRangeException(nameof(filterOperator), filterOperator, null)
+        };
+    }
+
+    private static IEnumerable<FilterByInfo> ParseFilterBy(string filterBy)
+    {
+        if (string.IsNullOrEmpty(filterBy)) yield break;
+
+        var items = filterBy.Split(',');
+        foreach (var item in items)
+        {
+            var pair = item.Trim().Split(' ');
+            if (pair.Length != 3)
+                throw new ArgumentException(
+                    $"Invalid FilterBy string '{item}'. Filter By Format: PropertyName Operator Value");
+
+            var propertyName = pair[0];
+            var filterOperator = Enum.Parse<FilterOperator>(pair[1], true);
+            var value = ParseValue(pair[2]);
+
+            yield return new FilterByInfo
+            {
+                PropertyName = propertyName,
+                Operator = filterOperator,
+                Value = value
+            };
+        }
+    }
+
+    private static object ParseValue(string value)
+    {
+        if (bool.TryParse(value, out var boolResult))
+            return boolResult;
+        if (int.TryParse(value, out var intResult))
+            return intResult;
+        if (float.TryParse(value, out var floatResult))
+            return floatResult;
+        if (decimal.TryParse(value, out var decimalResult))
+            return decimalResult;
+        return value;
+    }
+}
+
+internal class FilterByInfo
+{
+    public string PropertyName { get; init; } = string.Empty;
+    public object Value { get; init; } = string.Empty;
+    public FilterOperator Operator { get; init; }
+}
+
+public enum FilterOperator
+{
+    Equal,
+    NotEqual,
+    GreaterThan,
+    GreaterThanOrEqual,
+    LessThan,
+    LessThanOrEqual,
+    Contains,
+    StartsWith,
+    EndsWith
+}
---
+++ Tests/ExtensionTests.cs
@@ -0,0 +1,209 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace GenericRepository.Tests;
+
+internal class TestEntity
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required bool IsActive { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public required float Price { get; set; }
+    public required decimal Amount { get; set; }
+}
+
+public class ExtensionTests(ITestOutputHelper testOutputHelper)
+{
+    private readonly List<TestEntity> _collection =
+    [
+        new()
+        {
+            Id = 1,
+            Name = "A",
+            IsActive = true,
+            CreatedAt = DateTime.Now,
+            Price = 10.5f,
+            Amount = 10.5m
+        },
+        new()
+        {
+            Id = 2,
+            Name = "B",
+            IsActive = true,
+            CreatedAt = DateTime.Now,
+            Price = 20.5f,
+            Amount = 20.5m
+        },
+        new()
+        {
+            Id = 3,
+            Name = "C",
+            IsActive = false,
+            CreatedAt = new DateTime(2022, 1, 1),
+            Price = 30.5f,
+            Amount = 30.5m
+        }
+    ];
+
+    [Fact]
+    public void OrderByTest()
+    {
+        // Arrange
+        var queryable = _collection.AsQueryable();
+
+        // Act
+        var result = queryable
+            .OrderBy("Name DESC")
+            .ToList();
+
+        // Assert
+        Assert.Equal("C", result[0].Name);
+        Assert.Equal("B", result[1].Name);
+        Assert.Equal("A", result[2].Name);
+
+        testOutputHelper.WriteLine("Ordered By Name DESC");
+        result.ForEach(x => testOutputHelper.WriteLine(x.Name));
+    }
+
+    [Fact]
+    public void FilterByTest()
+    {
+        // Arrange
+        var queryable = _collection.AsQueryable();
+
+        // Act
+        var result = queryable
+            .FilterBy("Name Equal B")
+            .ToList();
+
+        // Assert
+        Assert.Single(result);
+        Assert.Equal("B", result[0].Name);
+
+        testOutputHelper.WriteLine("Filtered By Name Equal B");
+        result.ForEach(x => testOutputHelper.WriteLine(x.Name));
+    }
+
+    // Filter By integer property
+    [Fact]
+    public void FilterById()
+    {
+        // Arrange
+        var queryable = _collection.AsQueryable();
+
+        // Act
+        var result = queryable
+            .FilterBy("Id GreaterThan 1")
+            .ToList();
+
+        // Assert
+        Assert.Equal(2, result.Count);
+        Assert.Equal("B", result[0].Name);
+        Assert.Equal("C", result[1].Name);
+
+        testOutputHelper.WriteLine("Filtered By Id GreaterThan 1");
+        result.ForEach(x => testOutputHelper.WriteLine(x.Name));
+    }
+
+    // Filter By boolean property
+    [Fact]
+    public void FilterByIsActive()
+    {
+        // Arrange
+        var queryable = _collection.AsQueryable();
+
+        // Act
+        var result = queryable
+            .FilterBy("IsActive Equal true")
+            .ToList();
+
+        // Assert
+        Assert.Equal(2, result.Count);
+        Assert.Equal("A", result[0].Name);
+        Assert.Equal("B", result[1].Name);
+
+        testOutputHelper.WriteLine("Filtered By IsActive Equal true");
+        result.ForEach(x => testOutputHelper.WriteLine(x.Name));
+    }
+
+    // Filter By DateTime property
+    [Fact]
+    public void FilterByCreatedAt()
+    {
+        // Arrange
+        var queryable = _collection.AsQueryable();
+
+        // Act
+        var result = queryable
+            .FilterBy("CreatedAt GreaterThan 2022-01-01")
+            .ToList();
+
+        // Assert
+        Assert.Equal(2, result.Count);
+
+        testOutputHelper.WriteLine("Filtered By CreatedAt GreaterThan 2022-01-01");
+        result.ForEach(x => testOutputHelper.WriteLine(x.Name));
+    }
+
+    // Filter By float property
+    [Fact]
+    public void FilterByPrice()
+    {
+        // Arrange
+        var queryable = _collection.AsQueryable();
+
+        // Act
+        var result = queryable
+            .FilterBy("Price GreaterThan 20.5")
+            .ToList();
+
+        // Assert
+        Assert.Single(result);
+        Assert.Equal("C", result[0].Name);
+
+        testOutputHelper.WriteLine("Filtered By Price GreaterThan 20.5");
+        result.ForEach(x => testOutputHelper.WriteLine(x.Name));
+    }
+
+    // Filter By decimal property
+    [Fact]
+    public void FilterByAmount()
+    {
+        // Arrange
+        var queryable = _collection.AsQueryable();
+
+        // Act
+        var result = queryable
+            .FilterBy("Amount GreaterThan 20.5")
+            .ToList();
+
+        // Assert
+        Assert.Single(result);
+        Assert.Equal("C", result[0].Name);
+
+        testOutputHelper.WriteLine("Filtered By Amount GreaterThan 20.5");
+        result.ForEach(x => testOutputHelper.WriteLine(x.Name));
+    }
+
+    [Fact]
+    public void FilterByAndOrderByTest()
+    {
+        // Arrange
+        var queryable = _collection.AsQueryable();
+
+        // Act
+        var result = queryable
+            .FilterBy("Name NotEqual B")
+            .OrderBy("Name DESC")
+            .ToList();
+
+        // Assert
+        Assert.Equal(2, result.Count);
+        Assert.Equal("C", result[0].Name);
+        Assert.Equal("A", result[1].Name);
+
+        testOutputHelper.WriteLine("Filtered By Name NotEqual B and Ordered By Name DESC");
+        result.ForEach(x => testOutputHelper.WriteLine(x.Name));
+    }
+}
